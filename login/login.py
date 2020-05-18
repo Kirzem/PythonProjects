16220f4fff331a38082bdaa34f5983fb24ea9236
Kirzem <todd.putman@gmail.com>
2020-05-17 21:42:39 -0400

    Moved login.py into it's own directory

---
+++ login/login.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#
+# Simple login system. Take username and password, login, then write to a text
+# file. If user doesn't exist, allow them to create an account.    4/16/20
+
+from getpass import getpass
+
+
+print('Enter your username: ')
+username = input()
+
+f"Hello, {username}!"
+
+#print('Enter your password: ')
+#password = getpass()
+
+#if len(password) <= 4:
+#    print('Please choose a password longer than 4 characters.')
+    
+    
+    
+#login_info = username + ':' + password    # make info neat to put into a file
+#print (login_info)
+
+
